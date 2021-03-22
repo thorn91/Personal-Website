dbcd76230cf288f4cdd428b51c32067fb4006ac2
Thomas Horn <hortw1991@gmail.com>
2021-03-22 00:21:49 -0400

    fixed user dropdown clickin

--- web/components/Navigation.vue
+++ web/components/Navigation.vue
@@ -62,7 +62,7 @@
 				<!-- Profile dropdown -->
 				<div class="ml-3 relative">
 				<div>
-					<button type="button" class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu" aria-expanded="false" aria-haspopup="true">
+					<button type="button" class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu" aria-expanded="false" aria-haspopup="true" @click="showUserDrop=!showUserDrop">
 					<span class="sr-only">Open user menu</span>
 					<img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=tWwKaZUzKR&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
 					</button>
@@ -78,7 +78,7 @@
 					From: "transform opacity-100 scale-100"
 					To: "transform opacity-0 scale-95"
 				-->
-				<div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+				<div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" v-show="showUserDrop">
 					<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
 					<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
 					<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
@@ -105,6 +105,12 @@
 <script>
 export default {
 	name: 'Navigation',
+
+	data() {
+		return {
+			showUserDrop: false,
+		}
+	},
 
 }
 </script>
