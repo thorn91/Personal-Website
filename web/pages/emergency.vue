24150b472996a94ed05e343b337bad936592e55b
Thomas Horn <hortw1991@gmail.com>
2021-09-27 21:10:06 -0400

    added emergency contacts

---
+++ web/pages/emergency.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <h1>EMERGENCY CONTACTS</h1>
+      <h2>ALL CONTACTS HAVE COUNTRY CODE +1 (USA)</h2>
+      <br />
+      <br />
+      <h2>{{ ANNIE }}</h2>
+      <h2>{{ MOM }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "personal",
+  head() {
+    return {
+      title: "THOMAS HORN's EMERGENCY CONTACTS",
+    };
+  },
+
+  data() {
+    return {
+      ANNIE: "[Partner] Annie Paulukonis: (510) 846-2108",
+      MOM: "[Mother] Jane Horn: (706) 254-1326",
+    };
+  },
+
+  methods: {},
+
+  components: {},
+};
+</script>
+
+<style>
+</style>
